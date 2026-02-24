a2d8c1dddc098fc28fcc0306d57d90b322572f78
ALynch5 <124340181@umail.ucc.ie>
2026-02-24 10:48:54 +0000

    Added weather dashboard and templates

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    cities = [
+        {"name": "Cork", "temp": "10°C", "desc": "light drizzle"},
+        {"name": "Dublin", "temp": "8°C", "desc": "clear sky"},
+        {"name": "London", "temp": "11°C", "desc": "broken clouds"},
+        {"name": "Paris", "temp": "14°C", "desc": "broken clouds"},
+    ]
+    return render_template("index.html", cities=cities)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
