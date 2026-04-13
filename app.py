76658906a0886758d4007d6c23036ed601464d88
ALynch5 <124340181@umail.ucc.ie>
2026-04-13 13:13:15 +0100

    Remove .env file

--- app.py
+++ app.py
@@ -66,6 +66,13 @@ def api_joke():
 def health():
     return jsonify({"status": "OK"})
 
+@app.route("/status")
+def status():
+    return jsonify({
+        "database": "ok",
+        "api": "ok"
+    })
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
