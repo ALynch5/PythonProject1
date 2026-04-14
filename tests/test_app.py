2f2e4a6a8ff732dd207caa3868f10db5899094b4
ALynch5 <124340181@umail.ucc.ie>
2026-04-14 20:35:51 +0100

    adding tests and improving CI pipeline

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+from app import app
+
+def test_health():
+    client = app.test_client()
+    response = client.get("/health")
+    assert response.status_code == 200
+
+def test_status():
+    client = app.test_client()
+    response = client.get("/status")
+    assert response.status_code == 200
