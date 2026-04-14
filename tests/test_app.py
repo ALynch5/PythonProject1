c080b968608d88782d699dd326ac919ecd9c1bbd
124320846 <124320846@umail.ucc.ie>
2026-04-14 21:12:10 +0100

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,11 @@
-#Colin editing this here to finally double check, 14/04
+# Colin editing this here to finally double check, 14/04
 from app import app
 
 def test_health():
     client = app.test_client()
     response = client.get("/health")
     assert response.status_code == 200
+
 
 def test_status():
     client = app.test_client()
